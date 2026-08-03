cf2676369f3cfa6174aa96aacd30d01784b44203
OMG972 <st22388@howick.school.nz>
2026-08-03 13:30:22 +1200

    Today I added the GUI for settings.

--- assessment-quiz.py
+++ assessment-quiz.py
@@ -11,7 +11,7 @@ def quit():
 def settings():
     ""
 
-def run_quiz_easy():
+def run_quiz_easy(question):
     #Runs easy level quiz
     with open("assesment-quiz-easy.json", "r") as file:
         quiz_data = json.load(file)
@@ -22,10 +22,11 @@ def run_quiz_easy():
 
     #Prints questions
     for question_id, content in quiz_data.items():
-        print(f"\n{content['question']}")
+        question.config(text=content['question'])
+        question.update
         
         for choice in content['choices']:
-            print(f"- {choice}")
+            question.update()
         
         #Takes in users answer
         answer = input("Enter answer: ").strip()
@@ -46,6 +47,8 @@ def run_quiz_easy():
         run_quiz_medium()
     elif score < 3:
         print("Better luck next time")
+
+    
 
 
 def run_quiz_medium():
@@ -127,7 +130,7 @@ root.configure(bg="lightblue")
 
 def easy_gui():
     easy = Toplevel(root)
-    easy.geometry("320x300")
+    easy.geometry("320x330")
     easy.title("Easy quiz")
     easy.resizable(0,0)
     easy.configure(bg="Lightgreen")
@@ -141,43 +144,51 @@ def easy_gui():
     box1 = Entry(easy, justify=LEFT, font="Arial 22 bold")
     box1.grid(row=2, column=0)
 
+    #Button to enter answer
+    button_next = Button(easy, text = "Next", width = 10, bg = "Lightgreen", command = "")
+    button_next.grid(row = 3, column = 0, ipady =10, padx = 5, pady = 10)
+
     #Button to run settings
-    button_settings = Button(easy, text = "Settings", width = 10, bg = "Lightgreen", command = "")
-    button_settings.grid(row=3, column=0, ipady =10, padx = 5, pady = 10)
+    button_settings = Button(easy, text = "Settings", width = 10, bg = "Lightgreen", command = settings_gui)
+    button_settings.grid(row=4, column=0, ipady =10, padx = 5, pady = 10)
 
     #Button to quit the quiz
     button_quit = Button(easy, text = "Quit", width = 10, bg = "Lightgreen", command = quit)
-    button_quit.grid(row=4, column=0, ipady = 10, padx = 5, pady = 10)
+    button_quit.grid(row=5, column=0, ipady = 10, padx = 5, pady = 10)
 
 
 def medium_gui():
-    Medium = Toplevel(root)
-    Medium.geometry("320x300")
-    Medium.title("Medium quiz")
-    Medium.resizable(0,0)
-    Medium.configure(bg="Yellow")
+    medium = Toplevel(root)
+    medium.geometry("320x330")
+    medium.title("Medium quiz")
+    medium.resizable(0,0)
+    medium.configure(bg="Yellow")
     
-    lbl1 = Label(Medium, text="Medium quiz", font="Arial 22 bold", fg="Black", bg = "Yellow")
+    lbl1 = Label(medium, text="Medium quiz", font="Arial 22 bold", fg="Black", bg = "Yellow")
     lbl1.grid(row=0, column=0, pady=5, padx=10)
 
-    question = Label(Medium, text="", font="Arial 14 bold", fg="Black", bg = "Yellow")
+    question = Label(medium, text="", font="Arial 14 bold", fg="Black", bg = "Yellow")
     question.grid(row=1, column=0, ipady=10)
 
-    box1 = Entry(Medium, justify=LEFT, font="Arial 22 bold")
+    box1 = Entry(medium, justify=LEFT, font="Arial 22 bold")
     box1.grid(row=2, column=0)
 
+    #Button to enter answer
+    button_next = Button(medium, text = "Next", width = 10, bg = "Lightgreen", command = "")
+    button_next.grid(row = 3, column = 0, ipady =10, padx = 5, pady = 10)
+
     #Button to run settings
-    button_settings = Button(Medium, text = "Settings", width = 10, bg = "Yellow", command = "")
-    button_settings.grid(row=3, column=0, ipady =10, padx = 5, pady = 10)
+    button_settings = Button(medium, text = "Settings", width = 10, bg = "Lightgreen", command = settings_gui)
+    button_settings.grid(row=4, column=0, ipady =10, padx = 5, pady = 10)
 
     #Button to quit the quiz
-    button_quit = Button(Medium, text = "Quit", width = 10, bg = "Yellow", command = quit)
-    button_quit.grid(row=4, column=0, ipady = 10, padx = 5, pady = 10)
+    button_quit = Button(medium, text = "Quit", width = 10, bg = "Lightgreen", command = quit)
+    button_quit.grid(row=5, column=0, ipady = 10, padx = 5, pady = 10)
 
 
 def hard_gui():
     hard = Toplevel(root)
-    hard.geometry("320x300")
+    hard.geometry("320x330")
     hard.title("Hard quiz")
     hard.resizable(0,0)
     hard.configure(bg="Orange")
@@ -191,20 +202,43 @@ def hard_gui():
     box1 = Entry(hard, justify=LEFT, font="Arial 22 bold")
     box1.grid(row=2, column=0)
 
+    #Button to enter answer
+    button_next = Button(hard, text = "Next", width = 10, bg = "Lightgreen", command = "")
+    button_next.grid(row = 3, column = 0, ipady =10, padx = 5, pady = 10)
+
     #Button to run settings
-    button_settings = Button(hard, text = "Settings", width = 10, bg = "Orange", command = "")
-    button_settings.grid(row=3, column=0, ipady =10, padx = 5, pady = 10)
+    button_settings = Button(hard, text = "Settings", width = 10, bg = "Lightgreen", command = settings_gui)
+    button_settings.grid(row=4, column=0, ipady =10, padx = 5, pady = 10)
 
     #Button to quit the quiz
-    button_quit = Button(hard, text = "Quit", width = 10, bg = "Orange", command = quit)
-    button_quit.grid(row=4, column=0, ipady = 10, padx = 5, pady = 10)
+    button_quit = Button(hard, text = "Quit", width = 10, bg = "Lightgreen", command = quit)
+    button_quit.grid(row=5, column=0, ipady = 10, padx = 5, pady = 10)
 
+
+def settings_gui():
+    settings = Toplevel(root)
+    settings.geometry("320x330")
+    settings.title("Settings")
+    settings.resizable(0,0)
+    settings.configure(bg="Lightgrey")
+
+    lbl1 = Label(settings, text="Settings", font="Arial 22 bold", fg="Black", bg = "Lightgrey")
+    lbl1.grid(row=0, column=1, pady=5, padx=10)
+
+    button_darkmode = Button(settings, text = "Dark mode", width = 10, bg = "Lightgrey", command = "")
+    button_darkmode.grid(row=1, column=0, ipady = 10, padx = 5, pady = 10)
+
+    button_colourblind_mode = Button(settings, text = "Colour blind mode", width = 14, bg = "Lightgrey", command = "")
+    button_colourblind_mode.grid(row=1, column=1, ipady = 10, padx = 5, pady = 10)
+
+    button_quit = Button(settings, text = "Quit", width = 10, bg = "Lightgrey", command = quit)
+    button_quit.grid(row=1, column=2, ipady = 10, padx = 5, pady = 10)
 
 #Title
-lbl1 = Label(root, text="Knowledge quiz", font="Arial 22 bold", fg="Black", bg = "lightblue").place(x=140, y=0)
+lbl1 = Label(root, text="Knowledge quiz", font="Arial 22 bold", fg="Black", bg = "Lightblue").place(x=140, y=0)
 
 #Brief introduction to the quiz
-lbl2 = Label(root, text=f"Welcome to the knowledge quiz! This quiz is designed \n as a quick fun way to test your knowledge", font="Arial 14 bold", fg="Black", bg = "lightblue"). place(x=0, y=40)
+lbl2 = Label(root, text=f"Welcome to the knowledge quiz! This quiz is designed \n as a quick fun way to test your knowledge", font="Arial 14 bold", fg="Black", bg = "Lightblue"). place(x=0, y=40)
 
 #Button to run the quiz
 button_run_quiz = Button(root, text = "Begin quiz easy", width = 14, bg = "lightblue", command = easy_gui)
@@ -219,7 +253,7 @@ button_run_quiz_hard = Button(root, text = "Begin quiz hard", width = 14, bg = "
 button_run_quiz_hard.pack(side = LEFT, ipady = 10, padx = 5, pady = 10)
 
 #Button to run settings
-button_settings = Button(root, text = "Settings", width = 7, bg = "lightblue", command = "")
+button_settings = Button(root, text = "Settings", width = 7, bg = "lightblue", command = settings_gui)
 button_settings.pack(side = LEFT, ipady =10, padx = 5, pady = 10)
 
 #Button to quit the quiz
